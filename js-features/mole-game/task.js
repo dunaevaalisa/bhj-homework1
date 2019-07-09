91bd8494cf5131e5a231a82b90d9f7bebb915046
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-07-10 01:59:55 +0300

    Update task.js

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,25 +3,20 @@ const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
 
 if (hole.classList.contains( 'hole_has-mole' )) {
-    function getHole( index ) {
-        for (let i = 0; i < 2; i++) {
-        hole.onclick;
-        }
-        dead.textContent = dead.textContent + 1;
-        dead = dead.textContent;
-        return dead;
+  function getHole (index) {
+    for (let i = 0; i < 10; i++) {
+      hole.onclick;
+    }
+      dead.textContent ++;
+      return dead;
     }
 } else {
-    lost.textContent = lost.textContent + 1;
-    lost = lost.textContentж
+    lost.textContent ++;
     return lost;
 }
 
-console.log("Убито " + dead);
-console.log("Промахов " + lost);
-
-if (lost === 5) {
+if (lost = 5) {
     alert ("Вы проиграли");
-} else if (dead === 10) {
+} else if (dead = 10) {
     alert ("Победа!");
 }
