16f4a73cd35a6d86f4445ec4930ce6e99d838957
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-07-13 02:34:35 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,6 +2,11 @@ const cookie = document.getElementById ("cookie");
 const clicker__counter = document.getElementById ("clicker__counter");
 function changeCount() {
   clicker__counter.textContent ++;
-  cookie.width = 100;
+  if (clicker__counter.textContent % 2 == 0) {
+    cookie.width = 100;
+  } else {
+      cookie.width = 200;
+  }
 }
 cookie.onclick = changeCount;
+
